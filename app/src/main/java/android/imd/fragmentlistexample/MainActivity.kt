274db78b55a5dace740ac37400d0aa1630338dd0
Victor <victor_brandao@outlook.com>
2019-08-21 20:35:57 -0300

    Fixed

--- app/src/main/java/android/imd/fragmentlistexample/MainActivity.kt
+++ app/src/main/java/android/imd/fragmentlistexample/MainActivity.kt
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity(), AlimentoListFragment.OnItemClickAlimen
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textFragment = supportFragmentManager.findFragmentById(R.id.frag_text) as TextFragment
     }
 
     override fun onClick(alimento: Alimento) {
